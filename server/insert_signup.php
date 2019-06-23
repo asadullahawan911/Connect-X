a456cc922679f27b0dce4cefe90270e44063be5d
Asad Ullah Awan <asadullah.awan@ucp.edu.pk>
2019-06-23 12:51:16 +0500

    check for password included

--- server/insert_signup.php
+++ server/insert_signup.php
@@ -13,6 +13,11 @@ if(isset($_POST['signupbtn'])) {
     $password2 = mysqli_real_escape_string($conn, $_POST['psw-repeat']);
     $dob = mysqli_real_escape_string($conn, $_POST['dob']);
 }
+if (empty($username)) { array_push($errors, "Username is required"); }
+if (empty($email)) { array_push($errors, "Email is required"); }
+if (empty($password1)) { array_push($errors, "Password is required"); }
+if ($password1 != $password2) { array_push($errors, "The two passwords do not match");}
+
 $user_check_query = "SELECT * FROM user WHERE username='$username' OR email='$email' LIMIT 1";
 $result = mysqli_query($conn, $user_check_query);
 $user = mysqli_fetch_assoc($result);
@@ -27,7 +32,8 @@ if ($user) { // if user exists
     }
 }
 if (count($errors) == 0) {
-    $query = "INSERT INTO user (Username,email,password,RepeatPassword,rating,DOB,Profile_image) 
-  			  VALUES('$username', '$email', '$password1','$password2','0.0','$dob','')";
+    $password = $password1;
+    $query = "INSERT INTO user (username,email,password,rating,DOB,Profile_image) 
+  			  VALUES('$username', '$email', '$password','0.0','$dob','')";
     mysqli_query($conn, $query);
 }
