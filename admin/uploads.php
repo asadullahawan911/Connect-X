ded0abff3003566b769cd462eb199fdd088a12bd
Asad Ullah Awan <asadullah.awan@ucp.edu.pk>
2019-06-26 04:06:44 +0500

    authentication for admin and user done

--- admin/uploads.php
+++ admin/uploads.php
@@ -1,17 +1,25 @@
 <?php
+session_start();
 require_once "db_Connection.php";
-
+$userName = $_SESSION['uname'];
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
 if(isset($_POST["submit"])) {
 
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        $query = "INSERT INTO user (Profile_image) 
-  			  VALUES('$target_file')";
-        mysqli_query($conn, $query);
+        $query = "SELECT is_admin,username FROM user WHERE username='$userName'";
+        $result = mysqli_query($conn, $query);
+        $profileUser = mysqli_fetch_row($result);
 
-        header("location:userpanel.php");
+        $query= "UPDATE user SET Profile_image = '$target_file' WHERE user.username = '$profileUser[1]'";
+//        $query = "INSERT INTO user (Profile_image)
+//  			  VALUES('$target_file')";
+        mysqli_query($conn, $query);
+if($profileUser[0]=='yes')
+        header("location:adminpanel.php");
+else
+    header("location:userpanel.php");
     }
 }
 
