1fd1c8e67c714f6c7e4d8301dc020493519fd800
Rimsha Imran <rimshaimran@ucp.edu.pk>
2019-06-25 13:16:46 +0500

    session and cookies work done

--- server/insert_signup.php
+++ server/insert_signup.php
@@ -5,7 +5,6 @@ $password1="";
 $password2="";
 $dob="";
 $errors = array();
-
 if(isset($_POST['signupbtn'])) {
     $username = mysqli_real_escape_string($conn, $_POST['uname']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -13,21 +12,42 @@ if(isset($_POST['signupbtn'])) {
     $password2 = mysqli_real_escape_string($conn, $_POST['psw-repeat']);
     $dob = mysqli_real_escape_string($conn, $_POST['dob']);
 }
-$user_check_query = "SELECT * FROM user WHERE Username='$username' OR email='$email' LIMIT 1";
+if (empty($username)) { array_push($errors, "Username is required"); }
+if (empty($email)) { array_push($errors, "Email is required"); }
+if (empty($password1)) { array_push($errors, "Password is required"); }
+if ($password1 != $password2) { array_push($errors, "The two passwords do not match");}
+$user_check_query = "SELECT * FROM user WHERE username='$username' OR email='$email' LIMIT 1";
 $result = mysqli_query($conn, $user_check_query);
 $user = mysqli_fetch_assoc($result);
-
 if ($user) { // if user exists
-    if ($user['Username'] === $username) {
+    if ($user['username'] === $username) {
         array_push($errors, "Username already exists");
     }
-
     if ($user['email'] === $email) {
         array_push($errors, "email already exists");
     }
 }
 if (count($errors) == 0) {
-    $query = "INSERT INTO user (Username,email,password,RepeatPassword,rating,DOB,Profile_image) 
-  			  VALUES('$username', '$email', '$password1','$password2','0.0','$dob','')";
+    $password = $password1;
+    $query = "INSERT INTO user (username,email,password,rating,DOB,Profile_image) 
+  			  VALUES('$username', '$email', '$password','0.0','$dob','')";
     mysqli_query($conn, $query);
+    $results = mysqli_query($conn, $query);
+    if (mysqli_num_rows($results)==1) {
+        $_SESSION['uname']=$username;
+        $_SESSION['psw']=$password;
+        if(!empty($_POST['remember']))
+        {
+            setcookie('uname',$username,time()+(10*365*24*60*60));
+            setcookie('psw',$password,time()+(10*365*24*60*60));
+        }
+        else
+        {
+            setcookie('uname','' );
+            setcookie('psw', '');
+        }
+
+        header('location:index.php?logged_in=You have successfully signed in!');
+
+    }
 }
--- server/read_login.php
+++ server/read_login.php
@@ -2,7 +2,8 @@
 $username="";
 $password="";
 $errors = array();
-if (isset($_POST['loginbtn'])) {
+$msg='';
+if (isset($_POST['login'])) {
     $username = mysqli_real_escape_string($conn, $_POST['uname']);
     $password = mysqli_real_escape_string($conn, $_POST['psw']);
 
@@ -17,14 +18,27 @@ if (isset($_POST['loginbtn'])) {
 
         $query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
         $results = mysqli_query($conn, $query);
-        if (mysqli_num_rows($results) == 1) {
+        if (mysqli_num_rows($results)==1) {
+            $_SESSION['uname']=$username;
+            $_SESSION['psw']=$password;
+            if(!empty($_POST['remember']))
+            {
+                setcookie('uname',$username,time()+(10*365*24*60*60));
+                setcookie('psw',$password,time()+(10*365*24*60*60));
+            }
+            else
+            {
+                setcookie('uname','' );
+                setcookie('psw', '');
+            }
 
-           header('location: index.php');
+           header('location:index.php?logged_in=You have successfully logged in!');
 
         }
         else
             {
-            array_push($errors, "Wrong username/password combination");
+          array_push($errors, "Wrong username/password combination");
+            $msg='Wrong Email or Password. Try Again!';
         }
     }
 }
