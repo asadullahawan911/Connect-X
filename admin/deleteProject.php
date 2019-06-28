ae2ce8574cb32230c4eb6ff4b2c2c69269c9ee82
Asad Ullah Awan <asadullah.awan@ucp.edu.pk>
2019-06-28 19:25:20 +0500

    delete projects work done

--- admin/deleteProject.php
+++ admin/deleteProject.php
@@ -1,12 +1,9 @@
 <?php
-if(!isset($_SESSION['user_email'])){
-    header('location: login.php?not_admin=You are not Admin!');
-}
-if(isset($_GET['del_pro'])){
+include 'db_Connection.php';
     $del_id = $_GET['del_pro'];
     $del_pro = "delete from projects where Project_id='$del_id'";
-    $run_del = mysqli_query($con,$del_pro);
+    $run_del = mysqli_query($conn,$del_pro);
     if($run_del){
-        header('location: adminpanel.php');
+         header('location: adminpanel.php');
+
     }
-}
