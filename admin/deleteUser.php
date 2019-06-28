02c140a0b180fb4cffd966e474c68cc88b2f29cc
Asad Ullah Awan <asadullah.awan@ucp.edu.pk>
2019-06-28 19:25:34 +0500

    delete users work done

--- admin/deleteUser.php
+++ admin/deleteUser.php
@@ -1,1 +1,10 @@
 <?php
+include 'db_Connection.php';
+$user_id="";
+$user_id = $_GET['del_user'];
+$update_user = "delete from user where id='$user_id'";
+$update_data = mysqli_query($conn, $update_user);
+    if ($update_data) {
+         header("location: adminpanel.php");
+    }
+
